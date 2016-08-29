72bba2684d2194f6574c84eff07c6c0bbbb5f849
ForgottenGhost <my.name.is.longer.than.yours@hotmail.com>
2016-08-29 21:23:18 +0800

    Add files via upload

--- Board.py
+++ Board.py
@@ -1,15 +1,42 @@
+import pygame,pygame.gfxdraw
+from pygame.locals import *
+
 class board:
     def __init__(self,size,win_length):
+        self.background = Color(26,153,0)
+        self.line_width = 3
+        self.box_width = 5
         self.size = size
         self.win_length = win_length
         self.contain = [[0 for idx in xrange(self.size)] for jdx in xrange(self.size)]
+        self.selected_list = [(0,0),(1,0)]
+        self.surface_size = None
+        self.piece_size = None
+        self.gap = None
+        self.surface = None
 
-    def play_piece(self,loc,player):
-        if self.contain[loc[1]][loc[0]] == 0:
-            self.contain[loc[1]][loc[0]] = player
+    def play_piece(self,player):
+        if player == 1:
+            selected = self.selected_list[0]
+        elif player == -1:
+            selected = self.selected_list[1]
+        if self.contain[selected[1]][selected[0]] == 0:
+            self.contain[selected[1]][selected[0]] = player
             return True
         else:
             return False
+
+    def selecting(self,changing):
+        self.selected_list = [[self.selected_list[0][0]+changing[0][0],self.selected_list[0][1]+changing[0][1]],
+        [self.selected_list[1][0]+changing[1][0],self.selected_list[1][1]+changing[1][1]]]
+        for idx in xrange(len(self.selected_list)):
+            for jdx in xrange(len(self.selected_list[idx])):
+                if self.selected_list[idx][jdx] < 0:
+                    self.selected_list[idx][jdx] = 0
+                elif self.selected_list[idx][jdx] > self.size-1:
+                    self.selected_list[idx][jdx] = self.size-1
+        return [[0,0],[0,0]]
+
 
     def check_board(self):
         sum_list = [sum(self.contain[y][x:x+self.win_length]) for y in xrange(self.size) for x in xrange(self.size-self.win_length+1) if self.contain[y][x] != 0]
@@ -24,8 +51,32 @@ class board:
         else:
             return 0
 
-    def draw(self,screen,loc):
-        pass
+    def draw(self,surface_size):
+        if self.surface == None or self.surface != surface_size:
+            self.surface_size = surface_size
+            self.surface = pygame.Surface((self.surface_size, self.surface_size))
+            self.piece_size = int(self.surface_size/(2.0*self.size))-2
+            self.gap = (self.surface_size-(2.0*self.piece_size))/(self.size-1)
+        self.surface.fill(self.background)
+        for idx in xrange(self.size+1):
+            pygame.draw.aaline(self.surface, Color(0,0,0), (self.piece_size + (idx*self.gap),self.piece_size), (self.piece_size + (idx*self.gap),self.surface_size - self.piece_size), self.line_width)
+            pygame.draw.aaline(self.surface, Color(0,0,0), (self.piece_size,self.piece_size + (idx*self.gap)), (self.surface_size - self.piece_size,self.piece_size + (idx*self.gap)), self.line_width)
+        piece_list = [(self.piece_size + (x*self.gap),self.piece_size + (y*self.gap),self.contain[x][y])for x in xrange(self.size) for y in xrange(self.size) if self.contain[x][y] != 0]
+        for piece in piece_list:
+            if piece[2] == 1:
+                color = Color(0,0,0)
+            elif piece[2] == -1:
+                color = Color(255,255,255)
+            pygame.gfxdraw.aacircle(self.surface, int(piece[1]), int(piece[0]), self.piece_size, color)
+            pygame.gfxdraw.filled_circle(self.surface, int(piece[1]), int(piece[0]), self.piece_size, color)
+        if self.selected_list[0] != self.selected_list[1]:
+            for idx in xrange(len(self.selected_list)):
+                pygame.draw.rect(self.surface,Color(idx*255,idx*255,idx*255),
+                ((self.selected_list[idx][0]*self.gap,self.selected_list[idx][1]*self.gap),(self.piece_size*2,self.piece_size*2)),self.box_width)
+        else:
+            pygame.draw.rect(self.surface,Color(148,148,148),
+            ((self.selected_list[0][0]*self.gap,self.selected_list[0][1]*self.gap),(self.piece_size*2,self.piece_size*2)),self.box_width)
+        return self.surface
 
     def print_board(self):
         for idx in xrange(self.size):
--- Main.py
+++ Main.py
@@ -15,39 +15,78 @@ def select_index(info):
     return input_idx
 
 def draw():
-    global screen,player,board
-    screen.fill(background)
-    board.draw(screen)
+    global screen,player,board,clock,FPS,changing
+    time_passed = clock.tick(FPS)
+    changing = board.selecting(changing)
+    screen.fill(Color(0,0,0))
+    screen.blit(board.draw(screen_size[1]-(2*space)), (space, space))
     pygame.display.update()
 
 def main():
-    global screen,player,board
+    global screen,player,board,space,x_loc,y_loc,changing
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        #while board.play_piece([x_loc,y_loc],player) == False:
-            #x_loc = select_index("x location")
-            #y_loc = select_index("y location")
+            pressed_keys = pygame.key.get_pressed()
+            if pressed_keys[K_w]:
+                changing[0][1] -= 1
+            elif pressed_keys[K_s]:
+                changing[0][1] += 1
+            elif pressed_keys[K_a]:
+                changing[0][0] -= 1
+            elif pressed_keys[K_d]:
+                changing[0][0] += 1
+            if pressed_keys[K_SPACE] and player == 1:
+                changing = board.selecting(changing)
+                if board.play_piece(1):
+                    player = -player
+            if pressed_keys[K_UP]:
+                changing[1][1] -= 1
+            elif pressed_keys[K_DOWN]:
+                changing[1][1] += 1
+            elif pressed_keys[K_LEFT]:
+                changing[1][0] -= 1
+            elif pressed_keys[K_RIGHT]:
+                changing[1][0] += 1
+            if pressed_keys[K_RETURN] and player == -1:
+                changing = board.selecting(changing)
+                if board.play_piece(-1):
+                    player = -player
+                #if event.key == K_enter and player == -1:
+                #    changing = board.selecting(changing)
+                #    if board.play_piece(-1):
+                #        player = -player
+
+#        while board.play_piece([x_loc,y_loc],player) == False:
+#            x_loc = select_index("x location")
+#            y_loc = select_index("y location")
         #print "winner" + str(board.check_board())
-        player = -player
+#        player = -player
         #print "\n"
+#        time_passed += clock.tick()
+#        if
         draw()
 
 
 if __name__ == "__main__":
-    board_size = 10
+    board_size = 19
     screen_width = 640
-    background = (26,153,0)
     win_length = 3
-    player = -1
+    player = 1
+    space = 30
+    FPS = 30
     screen_size = (screen_width,screen_width)
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode(screen_size, 0, 32)
     pygame.display.set_caption("Connect6")
     board = board(board_size,win_length)
     board.print_board()
-    main()
+    changing = [[0,0],[0,0]]
+    x_loc = 0
+    y_loc = 0
     #x_loc = select_index("x location")
     #y_loc = select_index("y location")
+    main()
